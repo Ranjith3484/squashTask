5dbbe8dc9ae4976a837a88cbcc8c2d34955c2745
Ranjith3484 <54055196+Ranjith3484@users.noreply.github.com>
2020-09-25 14:41:15 +0530

    Add files via upload

---
+++ build/precache-manifest.646c107e62bd834c98477cab450f87e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3c4bbb5f2db4e7cd376e6d8dd988fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ae4a651a91652171d4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d16347e2ed7b0f8d2b07",
+    "url": "/static/js/2.906de92f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.906de92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ae4a651a91652171d4",
+    "url": "/static/js/main.d823bc0f.chunk.js"
+  },
+  {
+    "revision": "f65379ea21f6c07d0e4d",
+    "url": "/static/js/runtime-main.90b3f5e5.js"
+  }
+]);
